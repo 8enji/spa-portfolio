9fbecbb95efdb837d1c437c76c0d1779724e5a82
8enji <b.charest04@gmail.com>
2026-02-15 11:05:31 -0500

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import InteractiveGrid from "@/components/InteractiveGrid";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
     title: "Ben Charest",
@@ -17,6 +18,7 @@ export default function RootLayout({
             <body>
                 <InteractiveGrid />
                 {children}
+                <Analytics />
             </body>
         </html>
     );
